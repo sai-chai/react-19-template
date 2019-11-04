e8104ee7a922fa7354e0eb8c7b8b5d95075c53ce
Sai Chimata <saichimata@gmail.com>
2019-11-03 19:00:32 -0600

    added sass and styled components

--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,12 @@ module.exports = {
             ]
          },
          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+            test: /\.s[ac]ss$/i,
+            use: [
+               "style-loader",
+               "css-loader",
+               "sass-loader"
+            ]
          },
          {
             test: /\.(png|jpg|gif)$/,
